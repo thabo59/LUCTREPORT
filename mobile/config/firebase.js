b70773f763e46c5859382720847a332433906e2c
thabo59 <thabomonamane59@gmail.com>
2026-05-05 07:51:46 +0000

    Refactor Firebase config and initialization
    Updated Firebase configuration to include API keys and initialize app.

--- mobile/config/firebase.js
+++ mobile/config/firebase.js
@@ -3,7 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import firebaseConfig from "../../firebase-applet-config.json";
 
-const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyDcBkutzH4SueSAo1q0ZTBnrb_aWT4eONs",
+  authDomain: "luct-report.firebaseapp.com",
+  projectId: "luct-report",
+  storageBucket: "luct-report.firebasestorage.app",
+  messagingSenderId: "64541784225",
+  appId: "1:64541784225:web:bf0374c0bf2be9d26c18e0",
+  measurementId: "G-YKRWD48YQC"
+};
 
 export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
